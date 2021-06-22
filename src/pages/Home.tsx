3ab07e7e2b6639eb5e27370c18548b7821090f76
Nicolas Elena <nicolasdonizeteelena@gmail.com>
2021-06-22 16:13:41 -0300

    refactor: useAuth(hook) and AuthContext

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,7 @@
 import { useHistory } from 'react-router-dom';
 
+import { useAuth } from '../hooks/useAuth';
+
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
 import googleIconImg from '../assets/images/google-icon.svg';
@@ -7,12 +9,10 @@ import googleIconImg from '../assets/images/google-icon.svg';
 import { Button } from '../components/Button';
 
 import '../styles/auth.scss';
-import { useContext } from 'react';
-import { AuthContext } from '../App';
 
 export function Home() {
   const history = useHistory();
-  const { user, signInWithGoogle } = useContext(AuthContext);
+  const { user, signInWithGoogle } = useAuth();
 
   async function handleCreateRoom() {
     if (!user) {
